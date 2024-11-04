f0187b0376b9d26e637b89f672734a781bbffd8f
kraskoval <kr.muratovski@gmail.com>
2024-11-04 18:22:16 +0200

    Day 24: add more pages

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 df = pd.read_csv("topics.csv")
 pdf = FPDF(orientation='P', unit='mm', format='A4')
+
 for index, row in df.iterrows():
 	# print(row["Topic"])
 	pdf.add_page()
@@ -11,7 +12,8 @@ for index, row in df.iterrows():
 	pdf.set_text_color(255,0,0)
 	pdf.cell(w=0, h=12, text = f'{row["Topic"]}', align = "L",new_x=XPos.LMARGIN, new_y=YPos.NEXT)
 	pdf.line(10, 23, 200, 23)
-
+	for _ in range(row["Pages"] - 1):
+		pdf.add_page()
 
 pdf.output("output.pdf")
 # pdf.
