ab838bac76a4457611894c9e924e06f845ae68cf
kraskoval <kr.muratovski@gmail.com>
2024-11-04 20:26:28 +0200

    Day 24: add footers

--- main.py
+++ main.py
@@ -4,16 +4,30 @@ import pandas as pd
 
 df = pd.read_csv("topics.csv")
 pdf = FPDF(orientation='P', unit='mm', format='A4')
-
+pdf.set_auto_page_break(auto = False, margin=0 )
 for index, row in df.iterrows():
-	# print(row["Topic"])
 	pdf.add_page()
+
+	#Set the header
 	pdf.set_font('Times', 'B', 24)
 	pdf.set_text_color(255,0,0)
-	pdf.cell(w=0, h=12, text = f'{row["Topic"]}', align = "L",new_x=XPos.LMARGIN, new_y=YPos.NEXT)
+	pdf.cell(w=0, h=12, text = row["Topic"], align = "L",new_x=XPos.LMARGIN, new_y=YPos.NEXT)
 	pdf.line(10, 23, 200, 23)
+
+	#Set the footer
+	pdf.ln(265)
+	pdf.set_font('Times', 'I', 8)
+	pdf.set_text_color(180,180,180)
+	pdf.cell(w=0, h=12, text = row["Topic"], align = "R")
+
+
 	for _ in range(row["Pages"] - 1):
 		pdf.add_page()
+		# Set the footer
+		pdf.ln(277)
+		pdf.set_font('Times', 'I', 8)
+		pdf.set_text_color(180, 180, 180)
+		pdf.cell(w=0, h=12, text=row["Topic"], align="R")
 
 pdf.output("output.pdf")
 # pdf.
